8116cf8a5c1d9385ebfd3e90d2bc672c3a53a3ae
likelionSungGuk <sung9gi@gmail.com>
2021-03-09 21:48:07 +0900

    0309

--- 0308/BOJ_#1107리모컨.py
+++ 0308/BOJ_#1107리모컨.py
@@ -11,26 +11,19 @@ now = 100   #현위치
 
 if M != 0:
     broken = list(map(int, input().split()))    # 고장난 버튼들
-
-go_up = False        #올라가야 하는가? T/F
+else:
+    broken = []
 ans = abs(N-now)    #단순 무식하게 +/-로 이동하는 거리
-if N-now < 0: # N이 100보다 작음 -> --
-    go_up = True    #값을 올려주면서 체크
+
 
 target = N
-for i in range(500000):
+for i in range(1000000):
     flag = True
-    for j in str(target):
+    for j in str(i):
         if int(j) in broken:
             flag = False
-            if go_up:
-                target += 1
-            else:
-                target -= 1
-            print(target)
-        if flag == False:
             break
     #여기 왔다는 것은 숫자로 만들 수 있다는 것
     if flag == True:
-        ans = min(ans, i+len(list(str(N))))
+        ans = min(ans, len(str(i))+abs(N-i))
 print(ans)
