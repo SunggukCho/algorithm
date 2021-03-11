39affba613a3bb7a28e4441547d9f394d98e753a
likelionSungGuk <sung9gi@gmail.com>
2021-03-11 22:41:45 +0900

    0311

--- 0310/BOJ_#17141연구소2.py
+++ 0310/BOJ_#17141연구소2.py
@@ -5,26 +5,6 @@ BFS로 visited를 채운다.
 경우의 수를 구하고 각 경우의 수 마다 BFS를 돌린다.
 이때 모두 퍼져있는지 체크하고 (1), visited 값이 최소인 것을 갱신(2)한다.
 
-
-7 3
-2 0 0 0 1 1 0
-0 0 1 0 1 2 0
-0 1 1 0 1 0 0
-0 1 0 0 0 0 0
-0 0 0 2 0 1 1
-0 1 0 0 0 0 0
-2 1 0 0 0 0 2
--> 5
-
-7 3
-2 0 2 0 1 1 0
-0 0 1 0 1 2 0
-0 1 1 2 1 0 0
-2 1 0 0 0 0 2
-0 0 0 2 0 1 1
-0 1 0 0 0 0 0
-2 1 0 0 2 0 2
--> 5
 '''
 from itertools import combinations
 from collections import deque
@@ -39,11 +19,13 @@ def bfs(r, c):
         for k in range(4):
             nr = r+dr[k]
             nc = c+dc[k]
-            if 0 <= nr < N and 0 <= nc < N and lab[nr][nc] == 0:
-                if visited[nr][nc] > visited[r][c] + 1:
-                    Q.append((nr, nc))
-                    visited[nr][nc] = min(visited[r][c] + 1, visited[nr][nc])
-
+            if 0 <= nr < N and 0 <= nc < N and lab[nr][nc] != 1:
+                if visited[nr][nc] > cnt:
+                    Q.append((nr, nc, cnt+1))
+                    visited[nr][nc] = cnt + 1
+                elif visited[nr][nc] == -1:
+                    Q.append((nr, nc, cnt + 1))
+                    visited[nr][nc] = visited[r][c]+1
 
 
 N, M = map(int, input().split())
@@ -58,15 +40,30 @@ for i in range(N):
             walls.append((i, j))
 
 virus_combs = list(combinations(virus, M))
+result = -1
 for combs in virus_combs:
     visited = [[-1]*N for _ in range(N)]    #첫 출발을 0으로 둬야 하기 때문에 -1로 둔다.
+
     for x, y in walls:
         visited[x][y] = 0
-    print(combs)
 
     for comb in combs:
         r, c = comb
-        bfs(r,c)
-    for row in visited:
-        print(row)
-    print()
+        bfs(r, c)
+
+    flag = True #전부 다 퍼져있는가?
+    maxV = 0
+    for i in visited:
+        min_row = min(i)
+        if min_row == -1:
+            flag = False
+            break
+        else:
+            max_row = max(i)
+            maxV = max(max_row, maxV)
+
+    if flag:
+        if result > maxV or result == -1:
+            result = maxV
+
+print(result)
