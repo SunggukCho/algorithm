06f38483754e4c60b8b1d354c62cfb43ffff4ede
likelionSungGuk <sung9gi@gmail.com>
2021-03-19 23:05:13 +0900

    0319

--- 0318/BOJ_#15684사다리조작.py
+++ 0318/BOJ_#15684사다리조작.py
@@ -10,6 +10,11 @@ https://www.acmicpc.net/problem/15684
 -> 1
 '''
 
+N, M, H = map(int, input().split()) #세로선, 가로선, 다리 갯수
+arr = [[0]*N for _ in range()]
+result = 0 # M이 0인 경우 최종 답은 0
+if M != 0:
+    i, j = map(int, input().split())
 
 
 
---
+++ 0319/BOJ_#15685드래곤커브.py
@@ -0,0 +1,6 @@
+'''
+BOJ_#15685드래곤커브
+https://www.acmicpc.net/problem/15685
+
+
+'''
