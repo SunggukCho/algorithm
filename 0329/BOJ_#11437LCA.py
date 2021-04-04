b4295748d4b525492781308d2dc0b51d5206a419
likelionSungGuk <sung9gi@gmail.com>
2021-04-04 23:38:11 +0900

    kakao commerce javascript

--- 0329/BOJ_#11437LCA.py
+++ 0329/BOJ_#11437LCA.py
@@ -7,18 +7,16 @@ https://www.acmicpc.net/problem/11437
 만약 끝까지 다 돌면 루트가 공통노드.
 
 난관 1. 아니 ㅅㅂ input이 부모-자식순이 아니네? 2차원 배열로 받으면 N이 50000까지라서 메모리 무조건 터질거 같은데...
-난관 2.
+
 '''
 N = int(input())
 tree = [[] for _ in range(N+1)]
 for _ in range(N-1):
-    u, v = map(int, input().split()) #그냥 두 노드
+    u, v = map(int, input().split()) #그냥 두 개의 노드
     tree[u].append(v)
     tree[v].append(u)
-# print(G)
+
 M = int(input())
 for _ in range(M):
     x, y = map(int, input().split())
-
-
-
+    
